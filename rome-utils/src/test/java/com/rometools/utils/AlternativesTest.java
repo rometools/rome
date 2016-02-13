56a9150d957d1467436d959ba3fb443478a46879
mishako <mishako@users.noreply.github.com>
2016-02-13 18:37:48 +0100

    Move rome-utils files into a subdirectory

---
+++ rome-utils/src/test/java/com/rometools/utils/AlternativesTest.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rometools.utils;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class AlternativesTest {
+
+    @Test
+    public void testFirstNotNull() {
+
+        final Integer nullInteger = null;
+        final Integer notNullInteger = 1;
+
+        assertThat(Alternatives.firstNotNull(notNullInteger, nullInteger), is(notNullInteger));
+        assertThat(Alternatives.firstNotNull(nullInteger, notNullInteger), is(notNullInteger));
+        assertThat(Alternatives.firstNotNull(nullInteger, nullInteger), is(nullValue()));
+
+    }
+
+}
---
+++ rome-utils/src/test/java/com/rometools/utils/DatesTest.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rometools.utils;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class DatesTest {
+
+    @Test
+    public void testCopy() {
+
+        final Date date = new Date();
+        final Date nullDate = null;
+
+        assertThat(Dates.copy(date), is(notNullValue()));
+        assertThat(Dates.copy(date).getTime(), is(date.getTime()));
+        assertThat(Dates.copy(nullDate), is(nullValue()));
+
+    }
+
+}
