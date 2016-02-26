7a2805c9043e4344bef8e6f625615cd133b025d8
mishako <mishako@users.noreply.github.com>
2016-02-26 21:10:28 +0100

    Add OSGi integration tests

---
+++ rome-osgi-test/src/test/java/com/rometools/rome/osgi/FetcherIT.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rometools.rome.osgi;
+
+import static org.junit.Assert.assertEquals;
+
+import com.rometools.fetcher.FeedFetcher;
+import com.rometools.fetcher.impl.FeedFetcherCache;
+import com.rometools.fetcher.impl.HashMapFeedInfoCache;
+import com.rometools.fetcher.impl.HttpURLFeedFetcher;
+import com.rometools.rome.feed.synd.SyndFeed;
+
+import org.junit.Test;
+
+import java.net.URL;
+
+public class FetcherIT {
+
+    @Test
+    public void test() throws Exception {
+        FeedFetcherCache cache = HashMapFeedInfoCache.getInstance();
+        FeedFetcher fetcher = new HttpURLFeedFetcher(cache);
+        SyndFeed feed = fetcher.retrieveFeed(new URL("http://www.w3.org/blog/news/feed/atom"));
+
+        assertEquals("W3C News", feed.getTitle());
+    }
+}
---
+++ rome-osgi-test/src/test/java/com/rometools/rome/osgi/FetcherOsgiIT.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rometools.rome.osgi;
+
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.CoreOptions;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@RunWith(PaxExam.class)
+public class FetcherOsgiIT extends FetcherIT {
+
+    @Configuration
+    public Option[] configuration() throws IOException, URISyntaxException {
+        return CoreOptions.options(
+            CoreOptions.junitBundles(),
+            CoreOptions.wrappedBundle("file:target/lib/jdom2.jar"),
+            CoreOptions.bundle("file:target/lib/rome.jar"),
+            CoreOptions.bundle("file:target/lib/rome-fetcher.jar"));
+    }
+}
