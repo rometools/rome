82b7b04e59acaa5d01081457cbf455ed2efc74d5
mishako <mishako@users.noreply.github.com>
2019-09-09 20:09:01 +0200

    Remove dependency on assertj
    It seems to be incompatible with older java versions. It fails to
    compile with errors like this one:
    "[ERROR] [...]/rome/io/impl/DateParserTest.java:[42,17] cannot access
    java.util.function.Predicate"

--- rome/src/test/java/com/rometools/rome/io/impl/DateParserTest.java
+++ rome/src/test/java/com/rometools/rome/io/impl/DateParserTest.java
@@ -1,51 +1,26 @@
 package com.rometools.rome.io.impl;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Date;
 import java.util.Locale;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit tests for {@link DateParser}.
- * 
- * @author <a href="mailto:lutz.horn@posteo.de">Lutz Horn</a>
- *
- */
 public class DateParserTest {
 
-	@Before
-	public void before() {
-	}
+    @Test
+    public void parseW3CDateTimeIsOk() throws Exception {
+        assertEquals(
+                new Date(1000),
+                DateParser.parseW3CDateTime("1970-01-01T00:00:01+00:00", Locale.GERMANY)
+        );
+    }
 
-	@After
-	public void after() {
-	}
-
-	@Test
-	public void parseW3CDateTimeIsOk() {
-		Date result = DateParser.parseW3CDateTime("2019-06-07T15:27:13+02:00", Locale.GERMANY);
-
-		assertDateFields(result);
-	}
-
-	@Test
-	public void parseW3CDateTimeWithTrailingWhitespaceIsOk() {
-		Date result = DateParser.parseW3CDateTime("2019-06-07T15:27:13+02:00    ", Locale.GERMANY);
-
-		assertDateFields(result);
-	}
-
-	private void assertDateFields(Date result) {
-		assertThat(result).isNotNull();
-		assertThat(result).hasYear(2019);
-		assertThat(result).hasMonth(6);
-		assertThat(result).hasDayOfMonth(7);
-		assertThat(result).hasHourOfDay(15);
-		assertThat(result).hasMinute(27);
-		assertThat(result).hasSecond(13);
-	}
-
+    @Test
+    public void parseW3CDateTimeWithTrailingWhitespaceIsOk() throws Exception {
+        assertEquals(
+                new Date(1000),
+                DateParser.parseW3CDateTime("1970-01-01T00:00:01+00:00   ", Locale.GERMANY)
+        );
+    }
 }
