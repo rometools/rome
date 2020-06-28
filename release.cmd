04c8e792085d19d93b0585a031ffdd11fccd8bac
Patrick Gotthard <patrick@patrick-gotthard.de>
2020-06-28 14:16:09 +0200

    Fix missing versions in pom.xml (#453)

---
+++ release.cmd
@@ -0,0 +1,19 @@
+if [%1] == [] goto :missingParameter
+if [%2] == [] goto :missingParameter
+
+set releaseVersion=%1
+set nextVersion=%2
+
+call mvnw versions:set -DgenerateBackupPoms=false -DnewVersion=%releaseVersion% || goto :error
+call mvnw scm:checkin -DpushChanges=false "-Dmessage=Update version to %releaseVersion%" || goto :error
+call mvnw versions:set -DgenerateBackupPoms=false -DnewVersion=%nextVersion% || goto :error
+call mvnw scm:checkin -DpushChanges=false "-Dmessage=Update version to %nextVersion%" || goto :error
+
+exit /b 0
+
+:missingParameter
+echo "Usage: release <releaseVersion> <nextVersion>"
+exit /b 1
+
+:error
+exit /b %errorlevel%
