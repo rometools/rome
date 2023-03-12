013b571a98149bf20a2e427b6dd6e059747b3e6c
Antonio Santos Izaguirre <antoniosanct@gmail.com>
2023-03-12 01:22:24 +0100

    remove some imports
    Signed-off-by: Antonio Santos Izaguirre <antoniosanct@gmail.com>

--- rome/src/main/java/com/rometools/rome/feed/atom/Person.java
+++ rome/src/main/java/com/rometools/rome/feed/atom/Person.java
@@ -25,7 +25,6 @@ import java.util.stream.Stream;
 import com.rometools.rome.feed.impl.CloneableBean;
 import com.rometools.rome.feed.impl.EqualsBean;
 import com.rometools.rome.feed.impl.ToStringBean;
-import com.rometools.rome.feed.module.Extendable;
 import com.rometools.rome.feed.module.Module;
 import com.rometools.rome.feed.module.impl.ModuleUtils;
 import com.rometools.rome.feed.synd.SyndPerson;
@@ -34,7 +33,7 @@ import com.rometools.utils.Lists;
 /**
  * Bean for person elements of Atom feeds.
  */
-public class Person implements SyndPerson, Cloneable, Serializable, Extendable {
+public class Person implements SyndPerson, Serializable {
 
     private static final long serialVersionUID = 1L;
 
